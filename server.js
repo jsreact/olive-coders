fc1919f323eb8a07f127e3627e5a35c32ede4c1c
Anna Zhu <zhuanna93@gmail.com>
2016-03-21 16:23:43 -0700

    Updated Express server and .gitignore to fix deployment issue

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 5000
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
 
 app.use(express.static(__dirname))
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
 
-app.listen(port, 'localhost', function (err) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('Listening at http://localhost:5000');
-});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'))
+})
+
