d3957c159a37c209d4bbc6ffabb253f05e66276f
Anna Zhu <zhuanna93@gmail.com>
2016-03-23 13:25:05 -0700

    WIP: Integrating d3 visualization for repo data

--- src/components/GithubUser.js
+++ src/components/GithubUser.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import rd3 from 'react-d3'
+
+const PieChart = rd3.PieChart
 
 class GithubUser extends Component {
-  reposBySize () {
-    this.props.github.repos.forEach(function(currVal) {
-      console.log(currVal.name)
+  reposByStars () {
+    // data for d3 chart
+    let data = this.props.github.repos.map(function(repo, index) {
+      return {label: repo.name, value: repo.stargazers_count}
     })
+    return data
   }
   render () {
     const { github: { user, repos } } = this.props
     // practicing ES6 destructuring!
+
+    const pieData = this.reposByStars()
     if (user.name) {
       return (
         <div>
@@ -16,7 +23,16 @@ class GithubUser extends Component {
           <div>{user.location}</div>
           <img src={user.avatar_url} width='144' height='144'
               style={{ borderRadius: '200px' }} />
-          <div>{this.reposBySize.call(this)}</div>
+          <div>
+            <PieChart
+              data={pieData}
+              width={500}
+              height={400}
+              radius={100}
+              innerRadius={20}
+              sectorBorderColor="white"
+            />
+          </div>
         </div>
       )
     } else {
--- src/reducers/error.js
+++ src/reducers/error.js
@@ -4,6 +4,7 @@ const initialState = {
 
 export default function error(state = initialState, action) {
   if(action.type === 'DISPLAY_ERROR') {
+    // how can we use the info in action.payload?
     return { ...state, error: {
       message: 'Sorry that is not a valid username.'
     } }
